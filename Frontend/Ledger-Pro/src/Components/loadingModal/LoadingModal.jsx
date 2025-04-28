f909191255fb6e76686d4f6ebe78e493ecaa3b73
Jaedon Linen <jaedonlinen@Jaedons-MBP.lan>
2025-04-28 13:35:29 -0400

    editing loading screen

--- Frontend/Ledger-Pro/src/Components/loadingModal/LoadingModal.jsx
+++ Frontend/Ledger-Pro/src/Components/loadingModal/LoadingModal.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import myImage from "/src/assets/LP-logo-black.png";
+import React, { useState, useEffect } from 'react'
 import './LoadingModal.css'
 
 function LoadingModal() {
+
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 4000); // 5 seconds
+
+    return () => clearTimeout(timer); // cleanup in case component unmounts
+  }, []);
+
   return (
     <div className="modal-container loading" >
-        <span class="loader"></span>
+      <span class="loader"></span>
+      {showMessage && <p>Please allow 1-2 minutes, backend is booting up!</p>}
     </div>
   )
 }
